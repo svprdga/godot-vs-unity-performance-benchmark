37755da37923d9315ca4342b93bf57d9385ab56f
svprdga <svprdga@tutanota.com>
2020-04-28 16:44:03 +0100

    Initial commit

---
+++ GodotPerformance3D/Generator.gd
@@ -0,0 +1,35 @@
+extends Spatial
+
+const CUBE = preload("res://Cube.tscn")
+
+onready var text = get_node("/root/Spatial/Control/Text")
+
+var cubes = 0
+
+func _ready():
+	var timer = Timer.new()
+	timer.connect("timeout",self,"_on_timer_timeout")
+	add_child(timer)
+	timer.start(0.1)
+	
+	
+func _on_timer_timeout():
+	var instance = CUBE.instance()
+	add_child(instance)
+	
+	var x = 0
+	var z = 0
+	
+	if (randf() > 0.5):
+		x = randf() * 5
+		z = randf() * 5
+	else:
+		x = randf() * -5
+		z = randf() * -5
+		
+	instance.translate(Vector3(x, 0, z))
+	
+	cubes = cubes+1
+	
+	text.text = "Cubes: " + str(cubes) + "\nFPS: " + str(Engine.get_frames_per_second())
+
---
+++ UnityPerformance3D/Assets/Generator.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Generator : MonoBehaviour {
+
+    public GameObject cube;
+    public Text text;
+
+    float targetTime = 0.1f;
+    int cubes = 0;
+
+    const float fpsMeasurePeriod = 0.5f;
+    private int m_FpsAccumulator = 0;
+    private float m_FpsNextPeriod = 0;
+    private int m_CurrentFps;
+
+    void Update() {
+        targetTime -= Time.deltaTime;
+
+        // measure average frames per second
+        m_FpsAccumulator++;
+        if (Time.realtimeSinceStartup > m_FpsNextPeriod) {
+            m_CurrentFps = (int) (m_FpsAccumulator / fpsMeasurePeriod);
+            m_FpsAccumulator = 0;
+            m_FpsNextPeriod += fpsMeasurePeriod;
+        }
+
+        if (targetTime <= 0f) {
+
+            float x = 0f;
+            float z = 0f;
+
+            if (Random.value >= 0.5f) {
+                x = Random.value * 5;
+                z = Random.value * 5;
+            } else {
+                x = Random.value * -5;
+                z = Random.value * -5;
+            }
+
+            Vector3 vector = transform.position;
+            vector.x += x;
+            vector.z += z;
+
+            Instantiate(cube, vector, Quaternion.identity);
+            cubes++;
+
+            text.text = "Cubes: " + cubes + "\nFPS: " + m_CurrentFps;
+
+            targetTime = 0.1f;
+        }
+
+    }
+
+}
